50493c3987a29471c19a845a3e23f08debeb7604
Cristian <cristian123105@gmail.com>
2019-02-03 21:36:55 -0200

    Adicionado o botão de Reiniciar/Sair

--- Jogodavelha/src/jogodavelha2/pkg0/JogoTela.java
+++ Jogodavelha/src/jogodavelha2/pkg0/JogoTela.java
@@ -1,5 +1,9 @@
 package jogodavelha2.pkg0;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class JogoTela extends javax.swing.JFrame {
@@ -45,6 +49,11 @@ public class JogoTela extends javax.swing.JFrame {
         tgbtn7 = new javax.swing.JToggleButton();
         tgbtn8 = new javax.swing.JToggleButton();
         tgbtn9 = new javax.swing.JToggleButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        btnOldMatch = new javax.swing.JMenuItem();
+        btnReset = new javax.swing.JMenuItem();
+        btnExit = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -104,6 +113,36 @@ public class JogoTela extends javax.swing.JFrame {
             }
         });
 
+        jMenu1.setText("Jogo");
+
+        btnOldMatch.setText("Partidas passadas");
+        btnOldMatch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOldMatchActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnOldMatch);
+
+        btnReset.setText("Reiniciar");
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResetActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnReset);
+
+        btnExit.setText("Sair");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnExit);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -134,7 +173,7 @@ public class JogoTela extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(76, 76, 76)
+                .addGap(80, 80, 80)
                 .addComponent(jLabel1)
                 .addGap(70, 70, 70)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -151,7 +190,7 @@ public class JogoTela extends javax.swing.JFrame {
                     .addComponent(tgbtn7, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(tgbtn8, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(tgbtn9, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(149, Short.MAX_VALUE))
+                .addContainerGap(124, Short.MAX_VALUE))
         );
 
         pack();
@@ -213,6 +252,18 @@ public class JogoTela extends javax.swing.JFrame {
         result();
     }//GEN-LAST:event_tgbtn9ActionPerformed
 
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
+      reset();
+    }//GEN-LAST:event_btnResetActionPerformed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    private void btnOldMatchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOldMatchActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnOldMatchActionPerformed
+
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -224,7 +275,12 @@ public class JogoTela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem btnExit;
+    private javax.swing.JMenuItem btnOldMatch;
+    private javax.swing.JMenuItem btnReset;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JToggleButton tgbtn1;
     private javax.swing.JToggleButton tgbtn2;
     private javax.swing.JToggleButton tgbtn3;
@@ -256,54 +312,85 @@ public String PlayerM() {
 //-----------------Teste de vitória do User---------------------------------------------------------------------------------------------------------------
         if (btn[1].equalsIgnoreCase(user) && btn[2].equalsIgnoreCase(user) && btn[3].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
 
         if (btn[4].equalsIgnoreCase(user) && btn[5].equalsIgnoreCase(user) && btn[6].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[7].equalsIgnoreCase(user) && btn[8].equalsIgnoreCase(user) && btn[9].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[1].equalsIgnoreCase(user) && btn[4].equalsIgnoreCase(user) && btn[7].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[2].equalsIgnoreCase(user) && btn[5].equalsIgnoreCase(user) && btn[8].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[3].equalsIgnoreCase(user) && btn[6].equalsIgnoreCase(user) && btn[9].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[1].equalsIgnoreCase(user) && btn[5].equalsIgnoreCase(user) && btn[9].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         if (btn[3].equalsIgnoreCase(user) && btn[5].equalsIgnoreCase(user) && btn[7].equalsIgnoreCase(user)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user + " você venceu!");
+            reset();
         }
         //-----------Teste de vitória do User2---------------------------------------------------------------------------------------------------------------
         if (btn[1].equalsIgnoreCase(user2) && btn[2].equalsIgnoreCase(user2) && btn[3].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[4].equalsIgnoreCase(user2) && btn[5].equalsIgnoreCase(user2) && btn[6].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[7].equalsIgnoreCase(user2) && btn[8].equalsIgnoreCase(user2) && btn[9].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[1].equalsIgnoreCase(user2) && btn[4].equalsIgnoreCase(user2) && btn[7].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, user2 + "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[2].equalsIgnoreCase(user2) && btn[5].equalsIgnoreCase(user2) && btn[8].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, user2 + "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[3].equalsIgnoreCase(user2) && btn[6].equalsIgnoreCase(user2) && btn[9].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, user2 + "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[1].equalsIgnoreCase(user2) && btn[5].equalsIgnoreCase(user2) && btn[9].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, user2 + "Parabéns jogador " + user2 + " você venceu!");
+            reset();
         }
         if (btn[3].equalsIgnoreCase(user2) && btn[5].equalsIgnoreCase(user2) && btn[7].equalsIgnoreCase(user2)) {
             JOptionPane.showMessageDialog(null, user2 + "Parabéns jogador " + user2 + " você venceu!");
+            reset();
 
+        }
+
+    }
+
+    public void reset() {
+        int r = JOptionPane.showConfirmDialog(null, "Deseja jogar novamente?");
+        if (r == JOptionPane.YES_OPTION) {
+            for (int i = 0; i < 10;) {
+                btn[i] = "";
+                i++;
+            }
+            this.dispose();
+            new JogoTela().setVisible(true);
+        } else {
+            System.exit(0);
         }
 
     }
